dfaa4420e50d5b49be09d011415b2a2e6ef42390
I54m <35064677+I54m@users.noreply.github.com>
2020-12-30 16:21:07 +1300

    Reverted last 'fix' for RankTitleCommand, fine tuned easter egg effect

--- src/main/java/net/vorplex/core/listeners/InventoryClick.java
+++ src/main/java/net/vorplex/core/listeners/InventoryClick.java
@@ -1,5 +1,7 @@
 package net.vorplex.core.listeners;
 
+import net.vorplex.core.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -16,22 +18,24 @@ import org.bukkit.potion.PotionEffectType;
 public class InventoryClick implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onInventoryClick(InventoryClickEvent event) {
+    public void onInventoryClick(final InventoryClickEvent event) {
         ItemStack item = event.getCurrentItem();
         if (item == null) return;
         if (item.getType() == Material.GLASS && event.getSlotType() == InventoryType.SlotType.ARMOR && event.getSlot() == 39) {
-            Player player = (Player) event.getWhoClicked();
-            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 3, 5));
+            final Player player = (Player) event.getWhoClicked();
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 5));
             player.playSound(player.getLocation(), Sound.ITEM_ELYTRA_FLYING, 100, 1);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 3, 5));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 5, 5));
-            player.damage(1);
-            player.setFoodLevel(0);
-            player.sendMessage(ChatColor.RED + "Best to keep your helmet on! You don't want to suffocate, do you?");
-            event.setCancelled(true);
-            player.stopSound(Sound.ITEM_ELYTRA_FLYING);
-            player.setFoodLevel(20);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 5, 10));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 5));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 5));
+            player.damage(5);
+            player.setFoodLevel(1);
+            Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                player.sendMessage(ChatColor.RED + "Best to keep your helmet on! You don't want to suffocate, do you?");
+                event.setCancelled(true);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 150, 10));
+                player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 150, 10));
+                player.stopSound(Sound.ITEM_ELYTRA_FLYING);
+            }, 40);
         }
     }
 }
