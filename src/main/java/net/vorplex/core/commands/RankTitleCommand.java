2944dacb6ad25b70d9053cdf733c0bdd5bcd37fc
I54m <35064677+I54m@users.noreply.github.com>
2020-12-30 14:16:48 +1300

    Fixed ranks with same priority only showing one, fixed gui maxing out at 18 for some odd reason, added scroller inventory support

--- src/main/java/net/vorplex/core/commands/RankTitleCommand.java
+++ src/main/java/net/vorplex/core/commands/RankTitleCommand.java
@@ -6,6 +6,7 @@ import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryOptions;
 import net.vorplex.core.Main;
 import net.vorplex.core.objects.IconMenu;
+import net.vorplex.core.objects.ScrollerInventory;
 import net.vorplex.core.util.UserFetcher;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,10 +16,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 
 public class RankTitleCommand implements CommandExecutor {
 
-    private Main plugin = Main.getInstance();
+    private final Main plugin = Main.getInstance();
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -65,52 +65,73 @@ public class RankTitleCommand implements CommandExecutor {
             if (player.hasPermission("group." + possiblegroup.getName())){
                 SortedMap<Integer, String> GroupPrefixes = possiblegroup.getCachedData().getMetaData(queryOptions).getPrefixes();
                 for (int priority : GroupPrefixes.keySet()){
-                    if (priority < plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes")){
-                        prefixes.put(priority, GroupPrefixes.get(priority));
+                    if (priority < plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes")) {
+                        prefixes.put(prefixes.size() + 1, GroupPrefixes.get(priority));
                     }
                 }
             }
         }
-        if (prefixes.size() <= 1){
+        if (prefixes.size() <= 1) {
             player.sendMessage(plugin.prefix + ChatColor.RED + "You do not have any prefixes to change!");
             return false;
         }
-        IconMenu menu = new IconMenu(ChatColor.LIGHT_PURPLE + "Rank Titles", 1, (clicker, menu1, slot, item) -> {
-            if (clicker == player){
-                if (item.getItemMeta().hasDisplayName()){
-                    menu1.close(clicker);
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta removeprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes"));
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta addprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes") + " " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
-                    clicker.sendMessage(plugin.prefix + ChatColor.LIGHT_PURPLE + "Set your prefix to: " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
-                    return true;
+        if (prefixes.size() <= 56) {
+            IconMenu menu = new IconMenu(ChatColor.LIGHT_PURPLE + "Rank Titles", 1, (clicker, menu1, slot, item) -> {
+                if (clicker.equals(player)) {
+                    if (item.getItemMeta().hasDisplayName()) {
+                        menu1.close(clicker);
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta removeprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes"));
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta addprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes") + " " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
+                        clicker.sendMessage(plugin.prefix + ChatColor.LIGHT_PURPLE + "Set your prefix to: " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
+                        return true;
+                    }
+                }
+                return false;
+            });
+            int position = 0;
+            for (int id : (prefixes).navigableKeySet()) {
+                menu.addButton(position, new ItemStack(Material.PAPER, 1), ChatColor.translateAlternateColorCodes('&', prefixes.get(id)), ChatColor.LIGHT_PURPLE + "Click this to change your selected prefix to: ", ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', prefixes.get(id)));
+                position++;
+                if (position >= 54) {
+                    break;
+                } else if (position == 45) {
+                    menu.setSize(6);
+                } else if (position == 36) {
+                    menu.setSize(5);
+                } else if (position == 27) {
+                    menu.setSize(4);
+                } else if (position == 18) {
+                    menu.setSize(3);
+                } else if (position == 9) {
+                    menu.setSize(2);
                 }
             }
+            menu.open(player);
+            return true;
+        } else {
+            ArrayList<ItemStack> items = new ArrayList<>();
+            for (int id : (prefixes).navigableKeySet()) {
+                ItemStack item = new ItemStack(Material.PAPER, 1);
+                ItemMeta im = item.getItemMeta();
+                im.setDisplayName(ChatColor.translateAlternateColorCodes('&', prefixes.get(id)));
+                im.setLore(Arrays.asList(ChatColor.LIGHT_PURPLE + "Click this to change your selected prefix to: ", ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', prefixes.get(id))));
+                item.setItemMeta(im);
+                items.add(item);
+            }
+            ScrollerInventory.onClick onClick = (clicker, item, scrollerInventory) -> {
+                if (clicker.equals(player)) {
+                    if (item.getItemMeta().hasDisplayName()) {
+                        clicker.closeInventory();
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta removeprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes"));
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta addprefix " + plugin.getConfig().getInt("RankTitle.priority-to-add-prefixes") + " " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
+                        clicker.sendMessage(plugin.prefix + ChatColor.LIGHT_PURPLE + "Set your prefix to: " + ChatColor.translateAlternateColorCodes('&', item.getItemMeta().getDisplayName()));
+                        return true;
+                    }
+                }
+                return false;
+            };
+            new ScrollerInventory(items, ChatColor.LIGHT_PURPLE + "Rank Titles", onClick).open(player);
             return false;
-        });
-        int position = 0;
-        for (int priority : (prefixes).navigableKeySet()){
-            menu.addButton(position, new ItemStack(Material.PAPER, 1), ChatColor.translateAlternateColorCodes('&', prefixes.get(priority)), ChatColor.LIGHT_PURPLE + "Click this to change your selected prefix to: ", ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', prefixes.get(priority)));
-            position++;
-            if (position >= 9){
-                menu.setSize(2);
-            }
-            if (position >= 18){
-                menu.setSize(3);
-            }
-            if (position >= 27){
-                menu.setSize(4);
-            }
-            if (position >= 36){
-                menu.setSize(5);
-            }
-            if (position >= 45){
-                menu.setSize(6);
-            }
-            if (position >= 54){
-                break;
-            }
         }
-        menu.open(player);
-        return true;
     }
 }
