f20ee90a8cf132c8afe07bb8790932bb07ff5d01
I54m <54mpenguin@gmail.com>
2021-03-21 20:25:58 +1300

    Fixed Gifting to offline players and gifting removing multiple stacks of the item gifted

--- src/main/java/net/vorplex/core/commands/GiftCommand.java
+++ src/main/java/net/vorplex/core/commands/GiftCommand.java
@@ -3,6 +3,8 @@ package net.vorplex.core.commands;
 import net.vorplex.core.Main;
 import net.vorplex.core.objects.Gift;
 import net.vorplex.core.objects.IconMenu;
+import net.vorplex.core.util.NameFetcher;
+import net.vorplex.core.util.UUIDFetcher;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -16,6 +18,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class GiftCommand implements CommandExecutor {
 
@@ -45,49 +51,67 @@ public class GiftCommand implements CommandExecutor {
             return false;
         }
         Player receiver = Bukkit.getPlayer(strings[0]);
+        UUID receiverUUID;
         if (receiver == null) {
-            player.sendMessage(plugin.prefix + ChatColor.RED + "That is not an online player's name!");
-            return false;
-        }
-        if (receiver.getUniqueId().equals(player.getUniqueId())) {
+            UUIDFetcher uuidFetcher = new UUIDFetcher();
+            uuidFetcher.fetch(strings[0]);
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            Future<UUID> future = executorService.submit(uuidFetcher);
+            try {
+                receiverUUID = future.get(10, TimeUnit.SECONDS);
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(plugin.prefix + ChatColor.RED + "Unable to fetch player's uuid!");
+                executorService.shutdown();
+                return false;
+            }
+            executorService.shutdown();
+        } else receiverUUID = receiver.getUniqueId();
+        String receiverName = NameFetcher.getName(receiverUUID);
+
+        if (receiverUUID.equals(player.getUniqueId())) {
             player.sendMessage(plugin.prefix + ChatColor.RED + "You cannot send yourself a gift!");
             return false;
         }
-        if (plugin.gifts.containsKey(receiver.getUniqueId())) {
-            ArrayList<Gift> gifts = new ArrayList<>(plugin.gifts.get(receiver.getUniqueId()));
+        if (plugin.gifts.containsKey(receiverUUID)) {
+            ArrayList<Gift> gifts = new ArrayList<>(plugin.gifts.get(receiverUUID));
             for (Gift gift : gifts) {
                 if (gift.getSender().equals(player.getUniqueId())) {
-                    player.sendMessage(plugin.prefix + ChatColor.RED + receiver.getName() + " already has an unopened gift from you!");
+                    player.sendMessage(plugin.prefix + ChatColor.RED + receiverName + " already has an unopened gift from you!");
                     return false;
                 }
             }
         }
         inProgress.put(player.getUniqueId(), itemgift);
-        player.getInventory().remove(itemgift);
+        if (old)
+            player.getItemInHand().setAmount(0);
+        else
+            player.getInventory().getItemInMainHand().setAmount(0);
         player.updateInventory();
         final ItemStack deny = old ? new ItemStack(Material.valueOf("WOOL"), 1, (short) 14) : new ItemStack(Material.RED_STAINED_GLASS_PANE);
         final ItemStack confirm = old ? new ItemStack(Material.valueOf("WOOL"), 1, (short) 5) : new ItemStack(Material.LIME_STAINED_GLASS_PANE);
+        UUID finalReceiverUUID = receiverUUID;
         IconMenu menu = new IconMenu(ChatColor.RED + "/Gift confirmation", 3, (clicker, menu1, slot, item) -> {
             if (clicker == player) {
                 if (item == null || !item.hasItemMeta() || !item.getItemMeta().hasDisplayName()) return false;
                 if (item.getItemMeta().getDisplayName().contains(ChatColor.GREEN + "Confirm") && (old ? item.getType() == confirm.getType() && item.getDurability() == confirm.getDurability() : item.getType() == confirm.getType())) {
-                    clicker.sendMessage(plugin.prefix + ChatColor.GREEN + "Gifting item to " + receiver.getName());
+                    clicker.sendMessage(plugin.prefix + ChatColor.GREEN + "Gifting item to " + receiverName);
                     Gift gift = new Gift(itemgift, player.getUniqueId());
-                    if (plugin.gifts.containsKey(receiver.getUniqueId())) {
-                        ArrayList<Gift> gifts = new ArrayList<>(plugin.gifts.get(receiver.getUniqueId()));
+                    if (plugin.gifts.containsKey(finalReceiverUUID)) {
+                        ArrayList<Gift> gifts = new ArrayList<>(plugin.gifts.get(finalReceiverUUID));
                         gifts.add(gift);
-                        plugin.gifts.put(receiver.getUniqueId(), gifts);
+                        plugin.gifts.put(finalReceiverUUID, gifts);
                     } else {
                         ArrayList<Gift> gifts = new ArrayList<>();
                         gifts.add(gift);
-                        plugin.gifts.put(receiver.getUniqueId(), gifts);
+                        plugin.gifts.put(finalReceiverUUID, gifts);
                     }
-                    if (receiver.isOnline())
+                    if (receiver != null && receiver.isOnline())
                         receiver.sendMessage(plugin.prefix + ChatColor.GREEN + clicker.getName() + " has sent you a gift! Do /gifts to claim it!");
                     inProgress.remove(clicker.getUniqueId());
                     return true;
                 } else if (item.getItemMeta().getDisplayName().contains(ChatColor.RED + "Deny") && (old ? item.getType() == deny.getType() && item.getDurability() == deny.getDurability() : item.getType() == deny.getType())) {
-                    clicker.sendMessage(plugin.prefix + ChatColor.RED + "Cancelled gifting item to " + receiver.getName());
+                    clicker.sendMessage(plugin.prefix + ChatColor.RED + "Cancelled gifting item to " + receiverName);
                     if (old) player.getInventory().setItemInHand(inProgress.get(clicker.getUniqueId()));
                     else player.getInventory().setItemInMainHand(inProgress.get(clicker.getUniqueId()));
                     inProgress.remove(clicker.getUniqueId());
@@ -97,7 +121,7 @@ public class GiftCommand implements CommandExecutor {
             }
             return false;
         }, (closer, menu1) -> {
-            closer.sendMessage(plugin.prefix + ChatColor.RED + "Cancelled gifting item to " + receiver.getName());
+            closer.sendMessage(plugin.prefix + ChatColor.RED + "Cancelled gifting item to " + receiverName);
             if (old) closer.getInventory().setItemInHand(inProgress.get(closer.getUniqueId()));
             else closer.getInventory().setItemInMainHand(inProgress.get(closer.getUniqueId()));
             inProgress.remove(closer.getUniqueId());
@@ -110,7 +134,7 @@ public class GiftCommand implements CommandExecutor {
         menu.addButton(2, deny, ChatColor.RED + "Deny");
 
         menu.addButton(4, new ItemStack(Material.PAPER), ChatColor.RED + "WARNING!!!",
-                ChatColor.WHITE + "Confirming this action will", ChatColor.WHITE + "send " + receiver.getName() + " the item in",
+                ChatColor.WHITE + "Confirming this action will", ChatColor.WHITE + "send " + receiverName + " the item in",
                 ChatColor.WHITE + "your main hand (below).", ChatColor.RED + "This cannot be undone!!");
 
         menu.addButton(6, confirm, ChatColor.GREEN + "Confirm");
