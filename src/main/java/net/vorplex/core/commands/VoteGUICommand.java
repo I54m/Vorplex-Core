81db758ab030382d4b2635f3bd8235d7b5faa5b7
I54m <54mpenguin@gmail.com>
2021-03-23 15:29:01 +1300

    Fixed book gui in 1.15+ not working

--- src/main/java/net/vorplex/core/commands/VoteGUICommand.java
+++ src/main/java/net/vorplex/core/commands/VoteGUICommand.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import net.md_5.bungee.chat.ComponentSerializer;
 import net.vorplex.core.Main;
 import net.vorplex.core.util.BookUtils;
@@ -15,15 +16,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Method;
 import java.util.List;
 
 public class VoteGUICommand implements CommandExecutor {
-    private static Main plugin = Main.getInstance();
+    private static final Main plugin = Main.getInstance();
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
         if (plugin.getConfig().getString("VoteBookGUI.votelinks.1") == null) {
             commandSender.sendMessage("vote link #1 is null!!");
             return false;
@@ -39,33 +41,42 @@ public class VoteGUICommand implements CommandExecutor {
             ComponentBuilder text = new ComponentBuilder("        Vote").strikethrough(false).bold(true).color(ChatColor.DARK_GREEN)
                     .append("\n------------------").strikethrough(true).bold(false).color(ChatColor.BLACK)
                     .append("\nVote for the server to gain rewards!\n").strikethrough(false).color(ChatColor.BLACK);
-            for (String id : plugin.getConfig().getConfigurationSection("VoteBookGUI.votelinks").getKeys(false)){
+            for (String id : plugin.getConfig().getConfigurationSection("VoteBookGUI.votelinks").getKeys(false)) {
                 text.append("\n>> ").strikethrough(false).bold(true).color(ChatColor.BLACK)
                         .append("Link " + id).strikethrough(false).bold(false).color(ChatColor.DARK_GREEN)
                         .event(new ClickEvent(ClickEvent.Action.OPEN_URL, plugin.getConfig().getString("VoteBookGUI.votelinks." + id)))
-                        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Go to vote link " + id + "!").create()));
+                        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Go to vote link " + id + "!")));
             }
-            List<Object> pages;
-            try {
-                pages = (List<Object>) NMSUtils.getBukkitClass("inventory.CraftMetaBook").getDeclaredField("pages").get(bookMeta);
-            } catch (ReflectiveOperationException ex) {
-                ex.printStackTrace();
-                fallbackChatMethod(player);
-                return false;
-            }
-            for (Class<?> clazz : NMSUtils.getNMSClass("IChatBaseComponent").getDeclaredClasses()) {
+            if (plugin.old) {
+                List<Object> pages;
                 try {
-                    Method method = clazz.getDeclaredMethod("a", String.class);
-                    if (method != null) {
-                        pages.add(method.invoke(clazz, ComponentSerializer.toString(text.create())));
-                        break;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    pages = (List<Object>) NMSUtils.getBukkitClass("inventory.CraftMetaBook").getDeclaredField("pages").get(bookMeta);
+                } catch (ReflectiveOperationException ex) {
+                    ex.printStackTrace();
                     fallbackChatMethod(player);
                     return false;
                 }
-            }
+                for (Class<?> clazz : NMSUtils.getNMSClass("IChatBaseComponent").getDeclaredClasses()) {
+                    try {
+                        Method method = clazz.getDeclaredMethod("a", String.class);
+                        if (method != null) {
+                            pages.add(method.invoke(clazz, ComponentSerializer.toString(text.create())));
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        fallbackChatMethod(player);
+                        return false;
+                    }
+                }
+            } else
+                try {
+                    bookMeta.spigot().setPages(text.create());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    fallbackChatMethod(player);
+                    return true;
+                }
             bookMeta.setTitle("/vote GUI");
             bookMeta.setAuthor("Vorplex");
             book.setItemMeta(bookMeta);
@@ -85,7 +96,7 @@ public class VoteGUICommand implements CommandExecutor {
             player.spigot().sendMessage(new ComponentBuilder("\n>> ").strikethrough(false).bold(true).color(ChatColor.WHITE)
                     .append("Link " + id).strikethrough(false).bold(false).color(ChatColor.DARK_GREEN)
                     .event(new ClickEvent(ClickEvent.Action.OPEN_URL, plugin.getConfig().getString("VoteBookGUI.votelinks." + id)))
-                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Go to vote link " + id + "!").create())).create());
+                    .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Go to vote link " + id + "!"))).create());
         }
     }
 }
