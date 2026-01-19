b353b40cf49e8a0ac29fde56bf392ff9f7f2cc94
I54m <i54m@i54m.com>
2026-01-19 21:45:43 +1300

    Change queue subcommand to use timeunit

--- src/main/java/net/vorplex/core/commands/AutoRestartCommand.java
+++ src/main/java/net/vorplex/core/commands/AutoRestartCommand.java
@@ -1,9 +1,11 @@
 package net.vorplex.core.commands;
 
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.LongArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
@@ -21,7 +23,8 @@ import org.bukkit.command.CommandSender;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class AutoRestartCommand {
     private static final VorplexCore plugin = VorplexCore.getInstance();
@@ -33,8 +36,9 @@ public class AutoRestartCommand {
             .then(Commands.literal("now").executes(AutoRestartCommand::restartNow))
             .then(Commands.literal("queue")
                     .executes(AutoRestartCommand::queueRestart)
-                    .then(Commands.argument("minutes|hours", StringArgumentType.word())
-                            .then(Commands.argument("amount of time", IntegerArgumentType.integer(0, 59))
+                    .then(Commands.argument("timeunit", StringArgumentType.word())
+                            .suggests(AutoRestartCommand::getTimeUnitSuggestions)
+                            .then(Commands.argument("amount of time", LongArgumentType.longArg(1))
                                     .executes(AutoRestartCommand::queueRestart)
                             ))
             )
@@ -45,75 +49,45 @@ public class AutoRestartCommand {
             .executes(AutoRestartCommand::help)
             .build();
 
+    private static CompletableFuture<Suggestions> getTimeUnitSuggestions(final CommandContext<CommandSourceStack> ctx, final SuggestionsBuilder builder) {
+        for (TimeUnit timeunit : TimeUnit.values()) {
+            builder.suggest(timeunit.toString());
+        }
+        return builder.buildFuture();
+    }
+
 
     private static int restartNow(final CommandContext<CommandSourceStack> ctx) {
         final CommandSender sender = ctx.getSource().getSender();
-        AutoRestartScheduler.stop();
-        AutoRestartScheduler.scheduleNotify(autoRestartConfig, 600);
+        AutoRestartScheduler.scheduleReboot(620);
         sender.sendRichMessage(plugin.getPrefix() + "<green>Rebooting in 30 seconds!");
-        Audience audience = Audience.audience(Bukkit.getServer().getOnlinePlayers());
-        if (autoRestartConfig.notifyChatEnabled)
-            audience.sendMessage(plugin.getPrefix().append(Component.text("Server will reboot in 30 seconds!").color(NamedTextColor.GREEN)));
-        if (autoRestartConfig.notifyTitleEnabled)
-            audience.showTitle(Title.title(Component.text("Server will reboot in 30 seconds!").color(NamedTextColor.GREEN), Component.empty(),
-                    Title.Times.times(Duration.ofMillis(500), Duration.ofMillis(3000), Duration.ofMillis(1000))));
-        audience.playSound(autoRestartConfig.notifySound);
-
-        AutoRestartScheduler.tasks.add(plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
-            Audience audienceLater = Audience.audience(Bukkit.getServer().getOnlinePlayers());
-            if (autoRestartConfig.notifyChatEnabled)
-                audienceLater.sendMessage(plugin.getPrefix().append(Component.text("Server is now rebooting!").color(NamedTextColor.GREEN)));
-            if (autoRestartConfig.notifyTitleEnabled)
-                audienceLater.showTitle(Title.title(Component.text("Server is now rebooting").color(NamedTextColor.GREEN), Component.empty(),
-                        Title.Times.times(Duration.ofMillis(500), Duration.ofMillis(3000), Duration.ofMillis(1000))));
-            audienceLater.playSound(autoRestartConfig.notifySound);
-            plugin.getServer().restart();
-        }, 600));
         return Command.SINGLE_SUCCESS;
     }
 
     private static int queueRestart(final CommandContext<CommandSourceStack> ctx) {
         final CommandSender sender = ctx.getSource().getSender();
-        final String minuteHours;
-        final int amountOfTime;
+        final String timeunitString;
+        final long amountOfTime;
         try {
-            minuteHours = ctx.getArgument("minutes|hours", String.class);
-            amountOfTime = ctx.getArgument("amount of time", Integer.class);
+            timeunitString = ctx.getArgument("timeunit", String.class);
+            amountOfTime = ctx.getArgument("amount of time", Long.class);
         } catch (IllegalArgumentException iae) {
             sender.sendRichMessage(plugin.getPrefix() + "<red>Queue an auto reboot. Usage:");
-            sender.sendRichMessage(plugin.getPrefix() + "<red>/autorestart queue <minutes|hours> <amount of time> ");
+            sender.sendRichMessage(plugin.getPrefix() + "<red>/autorestart queue <timeunit> <amount of time> ");
             return Command.SINGLE_SUCCESS;
         }
+        TimeUnit timeUnit;
+        try {
+            timeUnit = TimeUnit.valueOf(timeunitString);
+        } catch (IllegalArgumentException iae) {
+            sender.sendRichMessage(plugin.getPrefix() + "<red>" + timeunitString + " is not a valid timeunit!");
+            return Command.SINGLE_SUCCESS;
+        }
+        AutoRestartScheduler.scheduleReboot(timeUnit, amountOfTime);
 
-        AutoRestartConfig config = new AutoRestartConfig();
-        config.schedule = new ArrayList<>();
-        if (minuteHours.equalsIgnoreCase("minutes") || minuteHours.equalsIgnoreCase("minute")) {
-            if (amountOfTime > 59) {
-                sender.sendRichMessage(plugin.getPrefix() + "<red>Minutes must be between 0 and 59");
-                return Command.SINGLE_SUCCESS;
-            }
-            config.schedule.add("0 0/" + amountOfTime + " * 1/1 * ? *");
-        } else if (minuteHours.equalsIgnoreCase("hours") || minuteHours.equalsIgnoreCase("hour")) {
-            if (amountOfTime > 23) {
-                sender.sendRichMessage(plugin.getPrefix() + "<red>Hours must be between 0 and 23");
-                return Command.SINGLE_SUCCESS;
-            }
-            config.schedule.add("0 0 0/" + amountOfTime + " 1/1 * ? *");
-        } else {
-            sender.sendRichMessage(plugin.getPrefix() + "<red>You can only queue a reboot between 1 minute and 23 hours!");
-            return Command.SINGLE_SUCCESS;
-        }
-        String time = amountOfTime + " " + minuteHours;
+        String time = amountOfTime + " " + timeunitString.toLowerCase();
         sender.sendRichMessage(plugin.getPrefix() + "<green>Queued auto reboot for <time> from now!", Placeholder.parsed("time", time));
-        AutoRestartScheduler.stop();
-        AutoRestartScheduler.start(config);
-        Audience audience = Audience.audience(Bukkit.getServer().getOnlinePlayers());
-        if (autoRestartConfig.notifyChatEnabled)
-            audience.sendMessage(plugin.getPrefix().append(Component.text("Server will now auto reboot " + time + " from now!")).color(NamedTextColor.GREEN));
-        if (autoRestartConfig.notifyTitleEnabled)
-            audience.showTitle(Title.title(Component.text("Server will now auto reboot ").color(NamedTextColor.GREEN), Component.text(time + " from now!").color(NamedTextColor.GREEN),
-                    Title.Times.times(Duration.ofMillis(500), Duration.ofMillis(3000), Duration.ofMillis(1000))));
-        audience.playSound(autoRestartConfig.notifySound);
+
         return Command.SINGLE_SUCCESS;
     }
 
@@ -131,7 +105,7 @@ public class AutoRestartCommand {
             AutoRestartScheduler.stop();
             Audience audience = Audience.audience(Bukkit.getServer().getOnlinePlayers());
             if (autoRestartConfig.notifyChatEnabled)
-                audience.sendMessage(plugin.getPrefix().append(Component.text("Reboot was aborted!").color(NamedTextColor.GREEN)));
+                audience.sendMessage(Component.text("Reboot was aborted!").color(NamedTextColor.GREEN));
             if (autoRestartConfig.notifyTitleEnabled)
                 audience.showTitle(Title.title(Component.text("Reboot was aborted!").color(NamedTextColor.GREEN), Component.empty(),
                         Title.Times.times(Duration.ofMillis(500), Duration.ofMillis(3000), Duration.ofMillis(1000))));
@@ -151,13 +125,13 @@ public class AutoRestartCommand {
     }
 
     private static int help(final CommandContext<CommandSourceStack> ctx) {
-        ctx.getSource().getSender().sendRichMessage("<st><white>|----------</st>" + plugin.getPrefix() + "<st><white>----------|</st><br>"
-                + "<white>  /autorestart now <gray>- Reboot the server in 30s.<br>"
-                + "<white>  /autorestart queue <gray>- Queue an auto reboot.<br>"
-                + "<white>  /autorestart start <gray>- Add all auto reboots to the queue.<br>"
-                + "<white>  /autorestart stop <gray>- Stop and remove all reboots from the queue.<br>"
-                + "<white>  /autorestart info <gray>- Get the time to the next auto reboot.<br>"
-                + "<white>  /autorestart help <gray>- View this help menu."
+        ctx.getSource().getSender().sendRichMessage("<st><white>|                     </st> " + plugin.getPrefix() + "<st><white>                    |</st><br>"
+                + "<light_purple> /autorestart <white>now <gray>- Reboot the server in 30s.<br>"
+                + "<light_purple> /autorestart <white>queue <gray>- Queue an auto reboot.<br>"
+                + "<light_purple> /autorestart <white>start <gray>- Add all auto reboots to the queue.<br>"
+                + "<light_purple> /autorestart <white>stop <gray>- Stop and remove all reboots from the queue.<br>"
+                + "<light_purple> /autorestart <white>info <gray>- Get the time to the next auto reboot.<br>"
+                + "<light_purple> /autorestart <white>help <gray>- View this help menu."
         );
         return Command.SINGLE_SUCCESS;
     }
