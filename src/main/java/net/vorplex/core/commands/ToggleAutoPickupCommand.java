4dcdf1c76d012b97e836d8ed238475523d4bdb4b
I54m <i54m@i54m.com>
2026-02-02 17:07:52 +1300

    Add ToggleAutoPickupCommand

---
+++ src/main/java/net/vorplex/core/commands/ToggleAutoPickupCommand.java
@@ -0,0 +1,34 @@
+package net.vorplex.core.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import net.vorplex.core.VorplexCore;
+import net.vorplex.core.autopickup.AutoPickupConfig;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ToggleAutoPickupCommand {
+
+    private static final VorplexCore plugin = VorplexCore.getInstance();
+    private static AutoPickupConfig autoPickupConfig = plugin.getAutoPickupConfig();
+
+    public static final LiteralCommandNode<CommandSourceStack> COMMAND_NODE = Commands.literal("toggleautopickup")
+            .requires(ctx -> ctx.getSender() instanceof Player && ctx.getSender().hasPermission("vorplexcore.autopickup"))
+            .executes(ToggleAutoPickupCommand::toggleCommand).build();
+
+    private static int toggleCommand(final CommandContext<CommandSourceStack> ctx) {
+        final CommandSender sender = ctx.getSource().getSender();
+        final Player player = (Player) sender;
+        if (autoPickupConfig.getDisabledPlayers().contains(player.getUniqueId())) {
+            autoPickupConfig.getDisabledPlayers().remove(player.getUniqueId());
+            player.sendRichMessage("<green>Toggled Auto Pickup On!");
+        } else {
+            autoPickupConfig.getDisabledPlayers().add(player.getUniqueId());
+            player.sendRichMessage("<red>Toggled Auto Pickup off!");
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+}
