a6b0f8a4f94a851b077e25ab9ec7ad95574a4226
I54m <i54m@i54m.com>
2026-02-03 13:39:15 +1300

    Bump Config Version to 1.2

--- src/main/java/net/vorplex/core/util/ConfigUpdater.java
+++ src/main/java/net/vorplex/core/util/ConfigUpdater.java
@@ -19,7 +19,7 @@ public class ConfigUpdater {
     /**
      * Hard-coded current config version used in this version of the plugin
      */
-    private static final String CURRENT_CONFIG_VERSION = "1.1";
+    private static final String CURRENT_CONFIG_VERSION = "1.2";
 
     /**
      * Main usage for ConfigUpdater.
