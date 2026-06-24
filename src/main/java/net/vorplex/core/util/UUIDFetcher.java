a14e1ad2637593c33e04e82962b1fa6e26720149
I54m <i54m@i54m.com>
2026-06-25 02:02:58 +1200

    UUIDFetcher re-write - breaks 2x unit tests

--- src/main/java/net/vorplex/core/util/UUIDFetcher.java
+++ src/main/java/net/vorplex/core/util/UUIDFetcher.java
@@ -2,85 +2,155 @@ package net.vorplex.core.util;
 
 import com.google.gson.Gson;
 import lombok.Getter;
+import net.vorplex.core.VorplexCore;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.UUID;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.TimeUnit;
 
-//TODO fetcher update - try server cache first? - beginCall() & retrieveResult()
 public class UUIDFetcher implements Callable<UUID> {
 
-    @Getter
-    public static final UUID BLANK_UUID = UUID.fromString("0-0-0-0-0");
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
     private static final HashMap<String, UUID> UUIDS = new HashMap<>();
-    private String name;
 
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    public static void updateStoredUUID(String name, UUID uuid) {
+    }
+
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    public void fetch(String name) {
+    }
+
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    public void storeUUID(UUID uuid, String name) {
+    }
+
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    public UUIDFetcher() {
+    }
+
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    @Override
+    public UUID call() throws Exception {
+        return null;
+    }
+
+
+    @Getter
+    public static final UUID BLANK_UUID = new UUID(0L, 0L);
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(5))
+            .build();
+
+    private static final Gson GSON = new Gson();
+
+    /**
+     * Create an HTTP Request to the mojang api to fetch the player's UUID
+     *
+     * @param playerName the name of the player to fetch the uuid for
+     * @return the uuid of the player is fetched
+     * @throws Exception if any exceptions were encountered
+     */
+    private static UUID lookupUUID(String playerName) throws Exception {
+        if (playerName.equalsIgnoreCase("console"))
+            return BLANK_UUID;
+
+        UUID cachedUUID = getCachedUUID(playerName);
+        if (cachedUUID != null)
+            return cachedUUID;
+
+        if (!playerName.matches("[a-zA-Z0-9_]{1,16}"))
+            throw new IllegalArgumentException("Player name does not fit minecraft player name requirements!");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(
+                        "https://api.mojang.com/users/profiles/minecraft/" + playerName
+                ))
+                .timeout(Duration.ofSeconds(5))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200)
+            throw new IllegalStateException("Mojang API responded with a status code of: " + response.statusCode());
+
+        Profile profile = GSON.fromJson(response.body(), Profile.class);
+
+        if (profile == null || profile.id() == null)
+            throw new IllegalStateException("profile or profile.id() was null!");
+
+        UUID uuid = formatUUID(profile.id());
+
+        NameFetcher.storeName(uuid, profile.name());
+
+        return uuid;
+    }
+
+    /**
+     * get the Bukkit cache of a player's uuid or null if the player's uuid is not cached
+     *
+     * @param playerName the name of the player to get the uuid for
+     * @return the uuid of the player if in the cache
+     */
+    @Nullable
+    private static UUID getCachedUUID(String playerName) {
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayerIfCached(playerName);
+        return offlinePlayer == null ? null : offlinePlayer.getUniqueId();
+    }
+
+    private record Profile(String name, String id) {
+    }
+
+    /**
+     * Format a string into a UUID (with or without dashes)
+     *
+     * @param uuid the uuid string to format
+     * @return the formatted uuid
+     */
     public static UUID formatUUID(String uuid) {
         if (uuid.contains("-")) return UUID.fromString(uuid);
         else
             return UUID.fromString(uuid.replaceFirst("(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5"));
     }
 
-    public static void updateStoredUUID(String name, UUID uuid) {
-        for (String storedName : UUIDS.keySet()) {
-            UUID StoredUUID = UUIDS.get(storedName);
-            if (uuid.equals(StoredUUID)) {
-                if (!name.toLowerCase().equals(storedName)) {
-                    UUIDS.remove(storedName);
-                    UUIDS.put(name.toLowerCase(), uuid);
-                }
-                return;
+    /**
+     * Fetch a player's uuid Asynchronously
+     * use CompletableFuture#thenAccept() to run an async function once the uuid is fetched
+     *
+     * @param playerName name of player to fetch
+     * @return the UUID of the player
+     */
+    public static CompletableFuture<UUID> fetchUUIDAsync(String playerName) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return lookupUUID(playerName);
+            } catch (Exception e) {
+                throw new CompletionException(e);
             }
-        }
-        UUIDS.put(name.toLowerCase(), uuid);
+        }, VorplexCore.getInstance().getThreadPool());
     }
 
-    public void fetch(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public UUID call() throws Exception {
-        if (name.equalsIgnoreCase("console"))
-            return BLANK_UUID;
-        if (UUIDS.containsKey(name.toLowerCase()))
-            return UUIDS.get(name.toLowerCase());
-        StringBuilder sb = new StringBuilder();
-        URLConnection urlConn = new URI("https://api.mojang.com/users/profiles/minecraft/" + this.name).toURL().openConnection();
-        urlConn.setReadTimeout(5000);
-        if (urlConn.getInputStream() != null) {
-            InputStreamReader in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
-            BufferedReader bufferedReader = new BufferedReader(in);
-            if (bufferedReader.ready()) {
-                int cp;
-                while ((cp = bufferedReader.read()) != -1) {
-                    sb.append((char) cp);
-                }
-                bufferedReader.close();
-            }
-            in.close();
-        }
-        if (sb.toString().isEmpty() || sb.toString().length() < 7) return null;
-        Gson g = new Gson();
-        Profile profile;
-        profile = g.fromJson(sb.toString(), Profile.class);
-        UUID uuid = UUIDFetcher.formatUUID(profile.id);
-        NameFetcher.storeName(uuid, profile.name());
-        UUIDS.put(name.toLowerCase(), uuid);
-        return uuid;
-    }
-
-    public void storeUUID(UUID uuid, String name) {
-        UUIDS.put(name.toLowerCase(), uuid);
-    }
-
-
-    private record Profile(String name, String id) {
-
+    /**
+     * Fetch a player's uuid synchronously, with a 10-second timeout
+     *
+     * @param playerName name of player to fetch
+     * @return the UUID of the player
+     * @throws Exception any exception that was encountered during UUID fetching
+     */
+    public static UUID fetchUUID(String playerName) throws Exception {
+        return fetchUUIDAsync(playerName).get(10, TimeUnit.SECONDS);
     }
 }
