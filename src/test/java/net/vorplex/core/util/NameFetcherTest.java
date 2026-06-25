ba13d80dc95e8b2a84bf947b1c668aa80706da05
I54m <i54m@i54m.com>
2026-06-26 00:59:01 +1200

    Add NameFetcher Tests for NameFetcher re-write

--- src/test/java/net/vorplex/core/util/NameFetcherTest.java
+++ src/test/java/net/vorplex/core/util/NameFetcherTest.java
@@ -1,19 +1,63 @@
 package net.vorplex.core.util;
 
+import net.vorplex.core.util.profile.TestProfileCacheProvider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NameFetcherTest {
 
-    @Test
-    void getName() {
-        assertEquals("I54m", NameFetcher.getName("74f04a9bb7f9409da940b051f14dd3a5"));
+    private static ExecutorService threadPool;
+
+    @BeforeAll
+    static void setup() {
+        threadPool = Executors.newFixedThreadPool(4,
+                r -> {
+                    Thread thread = new Thread(r);
+                    thread.setName("VorplexCore-unitTests-Async");
+                    return thread;
+                });
+    }
+
+    @AfterAll
+    static void cleanup() {
+        threadPool.shutdownNow();
+    }
+
+    @BeforeEach
+    void eachSetup() {
+        NameFetcher.setCacheProvider(new TestProfileCacheProvider());
     }
 
     @Test
-    void getStoredName() {
-        NameFetcher.storeName(UUIDFetcher.formatUUID("74f04a9bb7f9409da940b051f14dd3a5"), "I54m");
-        assertEquals("I54m", NameFetcher.getName("74f04a9bb7f9409da940b051f14dd3a5"));
+    void fetchName_ConsoleUUID_ShouldReturnConsoleName() throws Exception {
+        String result = NameFetcher.fetchName(UUIDFetcher.getBLANK_UUID(), threadPool);
+        assertEquals("console", result);
+    }
+
+    @Test
+    void fetchNameAsync_ConsoleUUID_ShouldReturnConsoleName() throws Exception {
+        String result = NameFetcher.fetchNameAsync(UUIDFetcher.getBLANK_UUID(), threadPool).get();
+        assertEquals("console", result);
+    }
+
+    @Test
+    void fetchNameAsync_InvalidUUID_ShouldPropagateException() {
+        UUID invalidUuid = UUIDFetcher.formatUUID("c232ab00941411ecb3c89f6bdeced846");
+        assertThrows(CompletionException.class, () -> NameFetcher.fetchNameAsync(invalidUuid, threadPool).join());
+    }
+
+    @Test
+    void fetchName_ValidUUID_ShouldReturnExpectedName() throws Exception {
+        assertEquals("I54m", NameFetcher.fetchName(UUID.fromString("74f04a9b-b7f9-409d-a940-b051f14dd3a5"), threadPool));
     }
 }
