d2d9b1295d9cf8ed798d88c73181f6c30a30be7c
I54m <i54m@i54m.com>
2026-06-26 00:58:44 +1200

    NameFetcher re-write - breaks 2 unit tests

--- src/main/java/net/vorplex/core/util/NameFetcher.java
+++ src/main/java/net/vorplex/core/util/NameFetcher.java
@@ -1,80 +1,135 @@
 package net.vorplex.core.util;
 
 import com.google.gson.Gson;
+import lombok.Setter;
+import net.vorplex.core.VorplexCore;
+import net.vorplex.core.util.profile.BukkitProfileCacheProvider;
+import net.vorplex.core.util.profile.MojangProfile;
+import net.vorplex.core.util.profile.ProfileCacheProvider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-import java.util.HashMap;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
-//TODO fetcher update - try server cache first? - beginCall() & retrieveResult()
 public class NameFetcher {
-    private static final HashMap<UUID, String> NAMES = new HashMap<>();
-
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
     public static String getName(String uuid) {
-        return getName(UUIDFetcher.formatUUID(uuid));
+        return null;
     }
 
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
     public static String getName(UUID uuid) {
+        return null;
+    }
+
+    @Deprecated(since = "2.0-SNAPSHOT-1.4.2", forRemoval = true)
+    public static void updateStoredName(UUID uuid, String name) {
+    }
+
+
+    @Setter
+    private static ProfileCacheProvider cacheProvider = new BukkitProfileCacheProvider();
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(5))
+            .build();
+
+    private static final Gson GSON = new Gson();
+
+    /**
+     * Create an HTTP Request to the mojang session server api to fetch the player's name
+     *
+     * @param uuid the uuid of the player to fetch the uuid for
+     * @return the name of the player if fetched
+     * @throws Exception if any exceptions were encountered
+     */
+    private static String lookupName(UUID uuid) throws Exception {
         if (uuid.equals(UUIDFetcher.getBLANK_UUID()))
-            return "CONSOLE";
-        if (NAMES.containsKey(uuid))
-            return NAMES.get(uuid);
-        String output = callURL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.toString().replace("-", ""));
-        Gson g = new Gson();
-        Profile profile;
-        profile = g.fromJson(output.substring(0, output.indexOf(",\n  \"properties\"")) + "}", Profile.class);
-        new UUIDFetcher().storeUUID(uuid, profile.name());
-        NAMES.put(uuid, profile.name());
+            return "console";
+
+        String cachedName = cacheProvider.getCachedName(uuid);
+        if (cachedName != null)
+            return cachedName;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(
+                        "https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.toString().replace("-", "")
+                ))
+                .timeout(Duration.ofSeconds(5))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200)
+            throw new IllegalStateException("Mojang API responded with a status code of: " + response.statusCode());
+
+        MojangProfile profile = GSON.fromJson(response.body(), MojangProfile.class);
+
+        if (profile == null || profile.name() == null)
+            throw new IllegalStateException("profile or profile.name() was null!");
+
+        cacheProvider.storeProfile(profile);
+
         return profile.name();
     }
 
-    protected static String callURL(String URL) {
-        StringBuilder sb = new StringBuilder();
-        try {
-            URLConnection urlConn = new URI(URL).toURL().openConnection();
-            urlConn.setReadTimeout(6000);
-            if (urlConn.getInputStream() != null) {
-                InputStreamReader in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
-                BufferedReader bufferedReader = new BufferedReader(in);
-                if (bufferedReader.ready()) {
-                    int cp;
-                    while ((cp = bufferedReader.read()) != -1) {
-                        sb.append((char) cp);
-                    }
-                    bufferedReader.close();
-                }
-                in.close();
+    /**
+     * Fetch a player's name Asynchronously
+     * use CompletableFuture#thenAccept() to run an async function once the name is fetched
+     *
+     * @param uuid     uuid of player to fetch
+     * @param executor the thread executor to use
+     * @return the name of the player
+     */
+    public static CompletableFuture<String> fetchNameAsync(UUID uuid, Executor executor) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return lookupName(uuid);
+            } catch (Exception e) {
+                throw new CompletionException(e);
             }
-        } catch (IOException e) {
-            //ignore error
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
+        }, executor);
     }
 
-    public static void updateStoredName(UUID uuid, String name) {
-        storeName(uuid, name);
+    /**
+     * Fetch a player's name Asynchronously
+     * use CompletableFuture#thenAccept() to run an async function once the name is fetched
+     *
+     * @param uuid uuid of player to fetch
+     * @return the name of the player
+     */
+    public static CompletableFuture<String> fetchNameAsync(UUID uuid) {
+        return fetchNameAsync(uuid, VorplexCore.getInstance().getThreadPool());
     }
 
-    public static void storeName(UUID uuid, String name) {
-        NAMES.put(uuid, name);
+    /**
+     * Fetch a player's name synchronously, with a 10-second timeout
+     *
+     * @param uuid uuid of player to fetch
+     * @return the name of the player
+     * @throws Exception any exception that was encountered during name fetching
+     */
+    public static String fetchName(UUID uuid) throws Exception {
+        return fetchNameAsync(uuid, VorplexCore.getInstance().getThreadPool()).get(10, TimeUnit.SECONDS);
     }
 
-    public static String getStoredName(UUID uuid) {
-        return NAMES.get(uuid);
-    }
-
-    public static boolean hasNameStored(UUID uuid) {
-        return NAMES.containsKey(uuid);
-    }
-
-    protected record Profile(String name, String id) {
-
+    /**
+     * Fetch a player's name synchronously, with a 10-second timeout
+     *
+     * @param uuid     uuid of player to fetch
+     * @param executor the thread executor to use
+     * @return the name of the player
+     * @throws Exception any exception that was encountered during name fetching
+     */
+    public static String fetchName(UUID uuid, Executor executor) throws Exception {
+        return fetchNameAsync(uuid, executor).get(10, TimeUnit.SECONDS);
     }
 }
