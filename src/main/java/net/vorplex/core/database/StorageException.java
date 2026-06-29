8c398b42ecf57ce01f0204d4dfd68e124d92d6f8
I54m <i54m@i54m.com>
2026-06-29 16:46:38 +1200

    Add JavaDocs

--- src/main/java/net/vorplex/core/database/StorageException.java
+++ src/main/java/net/vorplex/core/database/StorageException.java
@@ -1,11 +1,24 @@
 package net.vorplex.core.database;
 
+/**
+ * Exception used to wrap any Exception encountered in a StorageProvider
+ */
 public class StorageException extends RuntimeException {
 
+    /**
+     * Basic constructor for a StorageException
+     *
+     * @param message The error message for the Exception
+     */
     public StorageException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor for a StorageException
+     * @param message The error message for the Exception
+     * @param cause The cause of the Exception (usually an SQLException)
+     */
     public StorageException(String message, Throwable cause) {
         super(message, cause);
     }
